1c6c39c2a18fe447aa222ef946f79bfc25bbae32
Elias Andualem <eandualem@gmail.com>
2017-06-20 04:36:53 +0300

    Website Structure

--- php/login.php
+++ php/login.php
@@ -6,3 +6,9 @@
  * Time: 9:48 PM
  */
 
+$name = $_GET['name'];
+$password = $_GET['password'];
+
+echo "your name is : <b> " . $name . " </b> <br>";
+echo "your password is : <b> " . $password . " </b> <br>";
+?>
